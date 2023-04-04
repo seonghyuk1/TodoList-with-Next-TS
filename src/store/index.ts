6dd018445f3331354dc62b1798d86d71a886bf58
seonghyuk1 <tjdgur0737@naver.com>
2023-04-04 17:57:30 +0900

    Redux Test

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,7 +19,7 @@ const rootReducer = combineReducers({
 
 // 합쳪진 리듀서에 타입이 '__NEXT_REDUX_WRAPPER_HYDRATE__'인 리듀서를 추가
 // Hydrate는 서버에서 생성된 리덕스 스토어를 클라이언트에서 사용할 수 있도록 전달
-const reducer = (state, action) => {
+const reducer = (state: any, action: any) => {
   if (action.type === HYDRATE) {
     const nextState = {
       ...state, // 이전상태
@@ -35,18 +35,18 @@ const reducer = (state, action) => {
 // 스토어 타입
 export type RootState = ReturnType<typeof rootReducer>;
 
-// * 미들웨어 적용을 위한 스토어 enhancer
-// 리덕스에서 미들웨어는 액션이 디스패치 되어 리듀서에서 처리하기 전에 사전에 지정된 작업들을 의미
-// 리덕스 데브툴 확장 프로그램을 사용하기 위해 미들웨어에 리덕스 데브툴을 사용하도록 하는 코드
-const bindMiddleware = (middleware: any) => {
-  if (process.env.NODE_ENV !== "production") {
-    const { composeWithDevTools } = require("redux-devtools-extension");
-    return composeWithDevTools(applyMiddleware(...middleware));
-  }
-  return applyMiddleware(...middleware);
-};
+// // * 미들웨어 적용을 위한 스토어 enhancer
+// // 리덕스에서 미들웨어는 액션이 디스패치 되어 리듀서에서 처리하기 전에 사전에 지정된 작업들을 의미
+// // 리덕스 데브툴 확장 프로그램을 사용하기 위해 미들웨어에 리덕스 데브툴을 사용하도록 하는 코드
+// const bindMiddleware = (middleware: any) => {
+//   if (process.env.NODE_ENV !== "production") {
+//     const { composeWithDevTools } = require("redux-devtools-extension");
+//     return composeWithDevTools(applyMiddleware(...middleware));
+//   }
+//   return applyMiddleware(...middleware);
+// };
 
-// 리듀서와 미들웨어로 리덕스 스토어를 만들어 리턴
+// // 리듀서와 미들웨어로 리덕스 스토어를 만들어 리턴
 
 const initStore = () => {
   // return createStore(reducer, bindMiddleware([]));
